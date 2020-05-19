402f38b48c9976189db3bd7eddfb8a2c5fecf11e
SergAlKoba <Serg.Al.Koba@gmail.com>
2020-05-19 10:46:22 +0300

    libra fix

--- assets/dist/js/libra.js
+++ assets/dist/js/libra.js
@@ -4,5 +4,29 @@ $(document).ready(function(){
         byRow: true
     });
 
+    var tableHeight = $('.fix thead').height();
+
+    $('.table-body').css({
+        'padding-top': tableHeight
+    });
+
+    $('.fix').css({
+        'height': tableHeight
+    });
+
+});
+
+
+$(window).resize(function(){
+
+    var tableHeight = $('.fix thead').height();
+
+    $('.table-body').css({
+        'padding-top': tableHeight
+    });
+
+    $('.fix').css({
+        'height': tableHeight
+    });
 
 });
