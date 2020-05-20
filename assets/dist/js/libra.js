5262f68d66d663bdc5caa6293ea9e35d64f68319
SergAlKoba <Serg.Al.Koba@gmail.com>
2020-05-20 11:13:14 +0300

    Go back table

--- assets/dist/js/libra.js
+++ assets/dist/js/libra.js
@@ -4,29 +4,5 @@ $(document).ready(function(){
         byRow: true
     });
 
-    var tableHeight = $('.fix thead').height();
-
-    $('.table-body').css({
-        'padding-top': tableHeight
-    });
-
-    $('.fix').css({
-        'height': tableHeight
-    });
-
-});
-
-
-$(window).resize(function(){
-
-    var tableHeight = $('.fix thead').height();
-
-    $('.table-body').css({
-        'padding-top': tableHeight
-    });
-
-    $('.fix').css({
-        'height': tableHeight
-    });
 
 });
